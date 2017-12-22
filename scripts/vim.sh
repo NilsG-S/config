213937a727ff54d7834d2b2ca6c4d929bbf2c643
NilsG-S <nils.griswold.steiner@gmail.com>
2017-12-22 06:28:30 -0600

    Added vim setup from source

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,3 +1,22 @@
+# Install Vim from source
+cd ~
+mkdir software
+cd software
+git clone https://github.com/vim/vim.git
+cd vim
+
+./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-python3interp=yes \
+    --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+    --enable-gui=gtk2 \
+    --enable-cscope \
+    --prefix=/usr/local
+
+make
+sudo make install
+# sudo make uninstall to remove
+
 # Downloading vim-plug
 # See: https://github.com/junegunn/vim-plug 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
