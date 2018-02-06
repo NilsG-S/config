2c801c7a90806b35cf73c276eacda7d9ebdb208b
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-06 12:35:27 -0600

    Added integrity checking for vim

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-# Install Vim from source
+echo -e "Creating `software` directory...\n"
+
+su nils
 cd ~
 mkdir software
 cd software
 
+echo -e "...Done\n"
+echo -e "Installing `ncurses-dev` and `python3-pip`...\n"
+
+sudo su
 apt install -y ncurses-dev python3-pip
+
+echo -e "...Done\n"
+echo -e "Installing `neovim`...\n"
+
+su nils
 pip3 install --upgrade pip
 pip3 install neovim
 
+echo -e "...Done\n"
+echo -e "Installing `Vim`...\n"
+
+# Install Vim from source
 git clone https://github.com/vim/vim.git
 cd vim
+git checkout tags/v8.0.1473 -b install
+git archive --format=tar -o ../check.tar.gz install
+sha=$(sudo git archive --format=tar install | sha256sum)
+found=$(grep -q 126cdf283ef7ebd8dc9aa13dd68fbd2aad4bfab75deb443eeba5606edf23ac14 <<< "$sha")
+
+if !found; then
+  echo "Vim source couldn't be verified"
+  exit 1
+fi
+
 ./configure \
   --with-features=huge \
   --enable-multibyte \
@@ -31,3 +56,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # Run GoInstallBinaries within Vim
 # Have to run `pip install neovim` for the appropriate version of Python
+
+echo -e "...Done\n"
