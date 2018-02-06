1ba1c123ea72b5888961a862d65a803f7deeff38
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-06 11:15:24 -0600

    Updated vim configuration script

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -4,21 +4,26 @@
 cd ~
 mkdir software
 cd software
+
+apt install -y ncurses-dev python3-pip
+pip3 install --upgrade pip
+pip3 install neovim
+
 git clone https://github.com/vim/vim.git
 cd vim
-
-./configure --with-features=huge \
-    --enable-multibyte \
-    --enable-python3interp=yes \
-    --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-    --enable-gui=gtk2 \
-    --enable-cscope \
-    --prefix=/usr/local
-
+./configure \
+  --with-features=huge \
+  --enable-multibyte \
+  --enable-python3interp=yes \
+  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+  --enable-gui=gtk2 \
+  --enable-cscope \
+  --prefix=/usr/local
 make
-sudo make install
+make install
 # sudo make uninstall to remove
 
+cd ~
 # Downloading vim-plug
 # See: https://github.com/junegunn/vim-plug 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -26,4 +31,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # Run GoInstallBinaries within Vim
 # Have to run `pip install neovim` for the appropriate version of Python
-
