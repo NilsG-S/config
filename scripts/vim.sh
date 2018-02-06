d3d3d47dba3a26899f93976f610adcc0858088df
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-06 17:04:19 -0600

    privilages fix

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,34 +2,30 @@
 
 echo -e "Creating \`software\` directory...\n"
 
-su nils
-cd ~
-mkdir software
-cd software
+sudo -u nils cd ~
+sudo -u nils mkdir software
+sudo -u nils cd software
 
 echo -e "...Done\n"
 echo -e "Installing \`ncurses-dev\` and \`python3-pip\`...\n"
 
-sudo su
 apt install -y ncurses-dev python3-pip
 
 echo -e "...Done\n"
 echo -e "Installing \`neovim\`...\n"
 
-su nils
-pip3 install --upgrade pip
-pip3 install neovim
+sudo -u nils pip3 install --upgrade pip
+sudo -u nils pip3 install neovim
 
 echo -e "...Done\n"
 echo -e "Installing \`Vim\`...\n"
 
 # Install Vim from source
-git clone https://github.com/vim/vim.git
-cd vim
+sudo -u nils git clone https://github.com/vim/vim.git
+sudo -u nils cd vim
 # Checking out specific release version
-git checkout tags/v8.0.1473 -b install
+sudo -u nils git checkout tags/v8.0.1473 -b install
 # Computing checksum for release version
-git archive --format=tar -o ../check.tar.gz install
 sha=$(git archive --format=tar install | sha256sum)
 found=$(grep -q 126cdf283ef7ebd8dc9aa13dd68fbd2aad4bfab75deb443eeba5606edf23ac14 <<< "$sha")
 
@@ -38,7 +34,6 @@ if !found; then
   exit 1
 fi
 
-sudo su
 ./configure \
   --with-features=huge \
   --enable-multibyte \
@@ -51,11 +46,10 @@ make
 make install
 # sudo make uninstall to remove
 
-su nils
-cd ~
+sudo -u nils cd ~
 # Downloading vim-plug
 # See: https://github.com/junegunn/vim-plug 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+sudo -u nils curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Run GoInstallBinaries within Vim
@@ -69,7 +63,7 @@ if [! -f ~/config/configs/vim/.vimrc] ; then
   exit 1
 fi
 
-cp ~/config/configs/vim/.vimrc ~/.vimrc
-cp -r ~/config/configs/vim/.vim ~/
+sudo -u nils cp ~/config/configs/vim/.vimrc ~/.vimrc
+sudo -u nils cp -r ~/config/configs/vim/.vim ~/
 
 echo -e "...Done\n"
