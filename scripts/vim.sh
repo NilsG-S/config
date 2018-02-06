30b7438724a6afb0af9e0f842f8b4c2275c628d9
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-06 17:20:23 -0600

    Fixed script errors

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,9 +2,9 @@
 
 echo -e "Creating \`software\` directory...\n"
 
-sudo -u nils cd ~
+cd ~
 sudo -u nils mkdir software
-sudo -u nils cd software
+cd software
 
 echo -e "...Done\n"
 echo -e "Installing \`ncurses-dev\` and \`python3-pip\`...\n"
@@ -12,24 +12,28 @@ echo -e "Installing \`ncurses-dev\` and \`python3-pip\`...\n"
 apt install -y ncurses-dev python3-pip
 
 echo -e "...Done\n"
+echo -e "Upgrading \`Pip\`...\n"
+
+pip3 install --upgrade pip
+
+echo -e "...Done\n"
 echo -e "Installing \`neovim\`...\n"
 
-sudo -u nils pip3 install --upgrade pip
-sudo -u nils pip3 install neovim
+pip3 install neovim
 
 echo -e "...Done\n"
 echo -e "Installing \`Vim\`...\n"
 
 # Install Vim from source
 sudo -u nils git clone https://github.com/vim/vim.git
-sudo -u nils cd vim
+cd vim
 # Checking out specific release version
 sudo -u nils git checkout tags/v8.0.1473 -b install
 # Computing checksum for release version
 sha=$(git archive --format=tar install | sha256sum)
 found=$(grep -q 126cdf283ef7ebd8dc9aa13dd68fbd2aad4bfab75deb443eeba5606edf23ac14 <<< "$sha")
 
-if !found; then
+if [ ! found ]; then
   echo "Vim source couldn't be verified"
   exit 1
 fi
@@ -46,7 +50,7 @@ make
 make install
 # sudo make uninstall to remove
 
-sudo -u nils cd ~
+cd ~
 # Downloading vim-plug
 # See: https://github.com/junegunn/vim-plug 
 sudo -u nils curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -58,7 +62,7 @@ sudo -u nils curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 echo -e "...Done\n"
 echo -e "Configuring \`Vim\`...\n"
 
-if [! -f ~/config/configs/vim/.vimrc] ; then
+if [ ! -f ~/config/configs/vim/.vimrc ] ; then
   echo "No configuration found"
   exit 1
 fi
