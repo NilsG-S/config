2ad47ed2739968cea602a8578775ff26181aa36e
NilsG-S <nils.griswold.steiner@gmail.com>
2018-03-06 19:59:28 -0600

    Got a start on translating vim.py

--- scripts/vim.py
+++ scripts/vim.py
@@ -5,10 +5,21 @@ def main():
     username = getpass.getuser()
     password = getpass.getpass()
 
-    print(username)
-    print(password)
+    with sh.contrib.sudo(password, _with=True)
+        print("Installing dependencies...")
 
-    sh.cd("scripts")
-    sh.touch("testing.txt")
+        sh.apt("install", "-y", "ncurses-dev")
+        sh.pip3("install", "neovim")
+        sh.pip3("install", "pylint")
+        sh.pip3("install", "jedi")
+
+    print("Installing Vim...")
+
+    sh.git("clone", "https://github.com/vim/vim.git")
+    sh.git("checkout", "tags/v8.0.1473", "-b", "install")
+
+    # TODO(NilsG-S): I'm not sure if this works...
+    sha = sh.sha256sum(sh.git("archive", "--format=tar", "install"))
+    found = sh.grep("-q", "126cdf283ef7ebd8dc9aa13dd68fbd2aad4bfab75deb443eeba5606edf23ac14", "<<<", sha)
 
 main()
